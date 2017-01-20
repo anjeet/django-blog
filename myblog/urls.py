ddf4ad47876c730e03cc29bffe01ecb5a0f66963
Anjeet <anjeetkaur5@gmail.com>
2017-01-21 03:18:41 +0530

    Login authentication added

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 )
