f8df4f4df92ee6298d74ba43d183a053105321cd
Anjeet Kaur <Anjeet Kaur>
2017-06-04 18:48:22 +0530

    done

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,28 @@
-from django.conf.urls import patterns, include, url
+
+from django.conf.urls import include, url
 from django.contrib import admin
+
 from django.contrib.auth import views
 
-
-urlpatterns = patterns(
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
-)
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
